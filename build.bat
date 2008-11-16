1dd90ffcab8ee74eaa1ac947e68dc6ed6f1702b8
Stefan Sundin <stefansundin@users.noreply.github.com>
2008-11-16 00:37:12 +0000

    Tagging release 0.6

--- build.bat
+++ build.bat
@@ -1,14 +1,12 @@
 
-taskkill /IM SuperF4.exe
-
-gcc -c hooks.c
-gcc -shared -o hooks.dll hooks.o
+gcc -c keyhook.c
+gcc -shared -o keyhook.dll keyhook.o
 
 windres -o resources.o resources.rc
-gcc -o SuperF4 superf4.c resources.o -mwindows -lshlwapi
+gcc -o SuperF4 superf4.c resources.o -mwindows
 
 strip SuperF4.exe
-strip hooks.dll
+strip keyhook.dll
 
 upx --best -qq SuperF4.exe
-upx --best -qq hooks.dll
+upx --best -qq keyhook.dll
