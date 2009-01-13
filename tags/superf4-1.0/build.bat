55af6c0489c365cc38d0ac749c725e81b821659a
recover89 <recover89@27437742-a845-0410-b1e9-6f0ddf50d4ec>
2009-01-13 14:20:27 +0000

    Tagging release 1.0

---
+++ tags/superf4-1.0/build.bat
@@ -0,0 +1,42 @@
+@echo off
+
+taskkill /IM SuperF4.exe
+
+if not exist build (
+	mkdir build
+)
+
+windres -o build/resources.o resources.rc
+
+if "%1" == "all" (
+	echo Building all
+	
+	for /D %%f in (localization/*) do (
+		@echo.
+		echo Building %%f
+		if not exist "build/%%f/SuperF4" (
+			mkdir "build\%%f\SuperF4"
+		)
+		copy "localization\%%f\info.txt" "build/%%f/SuperF4"
+		copy "SuperF4.ini" "build/%%f/SuperF4"
+		
+		gcc -o "build/%%f/SuperF4/SuperF4.exe" superf4.c build/resources.o -mwindows -lshlwapi -lwininet -DL10N_FILE=\"localization/%%f/strings.h\"
+		if exist "build/%%f/SuperF4/SuperF4.exe" (
+			strip "build/%%f/SuperF4/SuperF4.exe"
+			upx --best -qq "build/%%f/SuperF4/SuperF4.exe"
+		)
+	)
+	
+	@echo.
+	echo Building installer
+	makensis /V2 installer.nsi
+) else (
+	gcc -o SuperF4.exe superf4.c build/resources.o -mwindows -lshlwapi -lwininet -DDEBUG
+	
+	if "%1" == "run" (
+		start SuperF4.exe
+	)
+	if "%1" == "hide" (
+		start SuperF4.exe -hide
+	)
+)
---
+++ tags/superf4-1.0/localization/en-US/strings.h
@@ -0,0 +1,40 @@
+/*
+	SuperF4 - en-US localization by Stefan Sundin (recover89@gmail.com)
+	Do not localize APP_NAME, it will be automatically replaced.
+	Keep this file in UTF–8
+	
+	Copyright (C) 2008  Stefan Sundin (recover89@gmail.com)
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+*/
+
+//This will be updated when the strings in the program changes
+#define L10N_VERSION 1
+
+//Strings
+#define L10N_MENU_ENABLE       L"Enable"
+#define L10N_MENU_DISABLE      L"Disable"
+#define L10N_MENU_HIDE         L"Hide tray"
+#define L10N_MENU_AUTOSTART    L"Autostart"
+#define L10N_MENU_UPDATE       L"Update available"
+#define L10N_MENU_ABOUT        L"About"
+#define L10N_MENU_EXIT         L"Exit"
+#define L10N_TRAY_ENABLED      APP_NAME " (enabled)"
+#define L10N_TRAY_DISABLED     APP_NAME " (disabled)"
+#define L10N_UPDATE_BALLOON    L"New version found!"
+#define L10N_UPDATE_DIALOG     L"A new version is available.\nGo to website?"
+#define L10N_ABOUT_TITLE       L"About " APP_NAME
+#define L10N_ABOUT             APP_NAME " - " APP_VERSION "\n\
+Made by Stefan Sundin\n\
+\n\
+Press Ctrl+Alt+F4 to kill the process of the currently selected window.\n\
+The effect is the same as when you kill the process from the task manager.\n\
+\n\
+You can also kill a process by pressing [the windows key]+F4 and then clicking the window with the mouse.\n\
+You can press escape or the right mouse button to exit this mode without killing a program.\n\
+\n\
+Visit the website at "APP_URL"\n\
+Send feedback to recover89@gmail.com"
